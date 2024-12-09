40a8fc1375c31079bfe965575c3b6768c0856cac
Yamireek <Yamireek@users.noreply.github.com>
2024-12-09 17:08:15 +0100

    added engine project

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,10 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+
+import { engine } from '@card-engine-liesp/engine';
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="card-engine-lisp" />
-    </div>
-  );
+  return <div>{engine()}</div>;
 }
 
 export default App;
