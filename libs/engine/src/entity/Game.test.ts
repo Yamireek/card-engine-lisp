c69cfe7d316df73a4aed74c326dbb8d0330779c7
Yamireek <Yamireek@users.noreply.github.com>
2024-12-24 01:46:21 +0100

    testgame entity

--- libs/engine/src/entity/Game.test.ts
+++ libs/engine/src/entity/Game.test.ts
@@ -1,55 +1,44 @@
+import { Agent } from '../agent';
 import { InterpretedAgent } from '../agent/InterpretedAgent';
 import { StaticAgent } from '../agent/StaticAgent';
 import { Interpreter } from '../interpreter';
 import { toInstructions } from '../utils';
 import { Game } from './Game';
 
-it('create game', () => {
-  const game = new Game(new StaticAgent([]));
-  game.addCard({ name: 'HERO', type: 'hero', att: 2, def: 2 });
-  game.addCard({ name: 'ALLY', type: 'ally', att: 1, def: 1 });
-  game.addCard({ name: 'ENEMY', type: 'enemy', att: 3, def: 3 });
+class TestGame extends Game {
+  public counter = 1;
 
-  expect(game.cards[0].props.name).toBe('HERO');
-});
+  constructor(agent: Agent) {
+    super(agent);
+  }
+
+  runTest() {
+    this.counter = this.agent.chooseNumber(1, 5);
+  }
+}
 
 it('run method', () => {
-  const game = new Game(new StaticAgent([1]));
-  game.addCard({ name: 'HERO', type: 'hero', att: 2, def: 2 });
-  game.addCard({ name: 'ALLY', type: 'ally', att: 1, def: 1 });
-  game.addCard({ name: 'ENEMY', type: 'enemy', att: 3, def: 3 });
-
-  game.run();
-
-  expect(game.card[1].token.damage).toBe(0);
-  expect(game.card[2].token.damage).toBe(0);
-  expect(game.card[3].token.damage).toBe(1);
+  const game = new TestGame(new StaticAgent([2]));
+  game.runTest();
+  expect(game.counter).toBe(2);
 });
 
 it('interpret method', () => {
-  const game = new Game(new StaticAgent([3]));
-  game.addCard({ name: 'HERO', type: 'hero', att: 2, def: 2 });
-  game.addCard({ name: 'ALLY', type: 'ally', att: 1, def: 1 });
-  game.addCard({ name: 'ENEMY', type: 'enemy', att: 3, def: 3 });
-
-  const interpreter = new Interpreter(toInstructions('game.run()'), game);
+  const game = new TestGame(new StaticAgent([2]));
+  const interpreter = new Interpreter(toInstructions('game.runTest()'), game);
   interpreter.run();
-
-  expect(game.card[1].token.damage).toBe(0);
-  expect(game.card[2].token.damage).toBe(0);
-  expect(game.card[3].token.damage).toBe(3);
+  expect(game.counter).toBe(2);
 });
 
 it('real agent', () => {
-  const game = new Game(new InterpretedAgent());
+  const game = new TestGame(new InterpretedAgent());
   game.addCard({ name: 'HERO', type: 'hero', att: 2, def: 2 });
   game.addCard({ name: 'ALLY', type: 'ally', att: 1, def: 1 });
   game.addCard({ name: 'ENEMY', type: 'enemy', att: 3, def: 3 });
 
-  const interpreter = new Interpreter(toInstructions('game.run()'), game);
-  interpreter.run();
+  const interpreter = new Interpreter(toInstructions('game.runTest()'), game);
+  const result = interpreter.run();
 
-  expect(game.card[1].token.damage).toBe(0);
-  expect(game.card[2].token.damage).toBe(0);
-  expect(game.card[3].token.damage).toBe(3);
+  expect(game.counter).toBe(1);
+  expect(result).toHaveProperty('type', 'CHOICE');
 });
